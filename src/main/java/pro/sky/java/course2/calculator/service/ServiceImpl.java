fc996a0dcbd45d668265a5c6706f41d8b053b2d3
Диана <diana_yudina@mail.ru>
2022-01-10 21:22:50 +0500

    Corrected!

--- src/main/java/pro/sky/java/course2/calculator/service/ServiceImpl.java
+++ src/main/java/pro/sky/java/course2/calculator/service/ServiceImpl.java
@@ -3,11 +3,11 @@ package pro.sky.java.course2.calculator.service;
 
 public class ServiceImpl implements Service {
 
-    public int plus(int num1, int num2) { return num1 + num2; }
+    public int plus(int num1, int num2) { return num1 + num2;}
 
     public int minus(int num1, int num2) { return num1 - num2;}
 
     public int multy(int num1, int num2) { return num1 * num2;}
 
-    public int divide(int num1, int num2) { return num1 / num2; }
+    public int divide(int num1, int num2) { return num1 / num2;}
 }
